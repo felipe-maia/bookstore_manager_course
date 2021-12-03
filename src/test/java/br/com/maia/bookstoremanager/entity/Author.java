662ee6572bbb03700f37f466a2812c8a9cf5e159
Felipe Maia <fe_switch@hotmail.com>
2021-12-03 18:11:46 -0300

    mapeamento entity

---
+++ src/test/java/br/com/maia/bookstoremanager/entity/Author.java
@@ -0,0 +1,19 @@
+package br.com.maia.bookstoremanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+
+}
